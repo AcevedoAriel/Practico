c34e8ce57457f36eeac6c16ae147ddc286d2e9bc
AcevedoAriel <116052401+AcevedoAriel@users.noreply.github.com>
2023-08-25 20:25:25 -0300

    Add files via upload

---
+++ Practico 1/Practico 1/Formulario1.cs
@@ -0,0 +1,44 @@
+namespace Practico_1
+{
+    public partial class Formulario1 : Form
+    {
+        public Formulario1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = textBox1.Text + " " + textBox2.Text;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BEliminar_Click(object sender, EventArgs e)
+        {
+            textBox3.Clear();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Formulario1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //sale del formulario
+            if (e.Control && e.KeyCode == Keys.S)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void Formulario1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
